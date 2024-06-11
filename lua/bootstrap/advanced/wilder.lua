44719dd58274d26ea95676ac17411cee0ee2a89c
DrInfinite <work.naparajith@gmail.com>
2024-06-11 10:25:21 +0530

    feat: added wilder devicons

--- lua/bootstrap/advanced/wilder.lua
+++ lua/bootstrap/advanced/wilder.lua
@@ -7,16 +7,11 @@ local wilder = {
 		wilder.setup({ modes = { ":", "/", "?" } })
 		wilder.set_option(
 			"renderer",
-			wilder.wildmenu_renderer({
-				-- highlighter applies highlighting to the candidates
-				highlighter = wilder.basic_highlighter(),
-			})
-		)
-		wilder.set_option(
-			"renderer",
 			wilder.popupmenu_renderer({
 				-- highlighter applies highlighting to the candidates
 				highlighter = wilder.basic_highlighter(),
+				left = { " ", wilder.popupmenu_devicons() },
+				right = { " ", wilder.popupmenu_scrollbar() },
 			})
 		)
 	end,
