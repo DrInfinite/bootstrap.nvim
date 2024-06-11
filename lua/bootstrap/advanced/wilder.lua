936199e02772c75cb7f5832a062cc9f6acb32b7b
DrInfinite <work.naparajith@gmail.com>
2024-06-11 10:20:29 +0530

    fix: wilder display menu

--- lua/bootstrap/advanced/wilder.lua
+++ lua/bootstrap/advanced/wilder.lua
@@ -5,6 +5,13 @@ local wilder = {
 	config = function()
 		local wilder = require("wilder")
 		wilder.setup({ modes = { ":", "/", "?" } })
+		wilder.set_option(
+			"renderer",
+			wilder.wildmenu_renderer({
+				-- highlighter applies highlighting to the candidates
+				highlighter = wilder.basic_highlighter(),
+			})
+		)
 	end,
 }
 
