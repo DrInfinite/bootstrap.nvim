0176bc7cb25709ff237e466313d89a195c6d6b87
DrInfinite <work.naparajith@gmail.com>
2024-06-13 11:52:05 +0530

    fix(refactor): mini plugins refactor

--- lua/bootstrap/basic/mini.lua
+++
@@ -1,8 +0,0 @@
--- Collection of various small independent plugins/modules
-require("lua.bootstrap.basic.mini.ai")
-require("lua.bootstrap.basic.mini.diff")
-require("lua.bootstrap.basic.mini.indentscope")
-require("lua.bootstrap.basic.mini.pairs")
-
--- ... and there is more!
---  Check out: https://github.com/echasnovski/mini.nvim
