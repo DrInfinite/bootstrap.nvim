f423da87c75489fcc7d55085f66bbf25284a8cf7
DrInfinite <work.naparajith@gmail.com>
2024-06-13 10:40:04 +0530

    fix(refactor): moved mini and it's plugins to its own folder

--- init.lua
+++ init.lua
@@ -94,9 +94,7 @@ If you experience any errors while trying to install Bootstrap.nvim, run `:check
 This is all you need to get started traveller, enjoy your journey into the
 beautiful world of NeoVim.
 - Naparajith
-]]
-
--- Set <space> as the leader key
+]] -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
@@ -110,49 +108,45 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Install the latest stable release of lazy and setup lazy.nvim to use it
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--branch=stable", -- latest stable release
-		lazyrepo,
-		lazypath,
-	})
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", -- latest stable release
+    lazyrepo, lazypath})
 end
 
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-local imports = {
-	{ import = "bootstrap.basic" },
-	-- uncomment this line to add advanced plugins
-	{ import = "bootstrap.advanced" },
-	-- Uncomment this line while adding your custom plugins
-	-- { import = "bootstrap.custom" },
+local imports = {{
+    import = "bootstrap.basic"
+}, {
+    import = "bootstrap.mini"
+} -- uncomment this line to add advanced plugins
+-- { import = "bootstrap.advanced" },
+-- Uncomment this line while adding your custom plugins
+-- { import = "bootstrap.custom" },
 }
 
 local opts = {
-	ui = {
-		border = "single",
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+    ui = {
+        border = "single",
+        -- If you are using a Nerd Font: set icons to an empty table which will use the
+        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 "
+        }
+    }
 }
 
 require("lazy").setup(imports, opts)
--- lua/bootstrap/basic/mini.lua
+++ lua/bootstrap/basic/mini.lua
@@ -1,181 +1,193 @@
 local mini = { -- Collection of various small independent plugins/modules
-	"echasnovski/mini.nvim",
-	config = function()
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [']quote
-		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup( -- No need to copy this inside `setup()`. Will be used automatically.
-			{
-				-- Table with textobject id as fields, textobject specification as values.
-				-- Also use this to disable builtin textobjects. See |MiniAi.config|.
-				custom_textobjects = nil,
+    "echasnovski/mini.nvim",
+    config = function()
+        -- Work with diff hunks
+        require("mini.diff").setup({
+            -- Options for how hunks are visualized
+            view = {
+                -- Visualization style. Possible values are 'sign' and 'number'.
+                -- Default: 'number' if line numbers are enabled, 'sign' otherwise.
+                style = vim.go.number and "number" or "sign",
 
-				-- Module mappings. Use `''` (empty string) to disable one.
-				mappings = {
-					-- Main textobject prefixes
-					around = "a",
-					inside = "i",
+                -- Signs used for hunks with 'sign' view
+                signs = {
+                    add = "▒",
+                    change = "▒",
+                    delete = "▒"
+                },
 
-					-- Next/last variants
-					around_next = "an",
-					inside_next = "in",
-					around_last = "al",
-					inside_last = "il",
+                -- Priority of used visualization extmarks
+                priority = 199
+            },
 
-					-- Move cursor to corresponding edge of `a` textobject
-					goto_left = "g[",
-					goto_right = "g]",
-				},
+            -- Source for how reference text is computed/updated/etc
+            -- Uses content from Git index by default
+            source = nil,
 
-				-- Number of lines within which textobject is searched
-				n_lines = 500,
+            -- Delays (in ms) defining asynchronous processes
+            delay = {
+                -- How much to wait before update following every text change
+                text_change = 200
+            },
 
-				-- How to search for object (first inside current line, then inside
-				-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-				-- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-				search_method = "cover_or_next",
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                -- Apply hunks inside a visual/operator region
+                apply = "gh",
 
-				-- Whether to disable showing non-error feedback
-				silent = false,
-			}
-		)
-		-- Work with diff hunks
-		require("mini.diff").setup({
-			-- Options for how hunks are visualized
-			view = {
-				-- Visualization style. Possible values are 'sign' and 'number'.
-				-- Default: 'number' if line numbers are enabled, 'sign' otherwise.
-				style = vim.go.number and "number" or "sign",
+                -- Reset hunks inside a visual/operator region
+                reset = "gH",
 
-				-- Signs used for hunks with 'sign' view
-				signs = { add = "▒", change = "▒", delete = "▒" },
+                -- Hunk range textobject to be used inside operator
+                textobject = "gh",
 
-				-- Priority of used visualization extmarks
-				priority = 199,
-			},
+                -- Go to hunk range in corresponding direction
+                goto_first = "[H",
+                goto_prev = "[h",
+                goto_next = "]h",
+                goto_last = "]H"
+            },
 
-			-- Source for how reference text is computed/updated/etc
-			-- Uses content from Git index by default
-			source = nil,
+            -- Various options
+            options = {
+                -- Diff algorithm. See `:h vim.diff()`.
+                algorithm = "histogram",
 
-			-- Delays (in ms) defining asynchronous processes
-			delay = {
-				-- How much to wait before update following every text change
-				text_change = 200,
-			},
+                -- Whether to use "indent heuristic". See `:h vim.diff()`.
+                indent_heuristic = true,
 
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				-- Apply hunks inside a visual/operator region
-				apply = "gh",
+                -- The amount of second-stage diff to align lines (in Neovim>=0.9)
+                linematch = 60,
 
-				-- Reset hunks inside a visual/operator region
-				reset = "gH",
+                -- Whether to wrap around edges during hunk navigation
+                wrap_goto = false
+            }
+        })
+        -- Render Indent Guides
+        require("mini.indentscope").setup({
+            -- Draw options
+            draw = {
+                -- Delay (in ms) between event and start of drawing scope indicator
+                delay = 100,
 
-				-- Hunk range textobject to be used inside operator
-				textobject = "gh",
+                -- Animation rule for scope's first drawing. A function which, given
+                -- next and total step numbers, returns wait time (in ms). See
+                -- |MiniIndentscope.gen_animation| for builtin options. To disable
+                -- animation, use `require('mini.indentscope').gen_animation.none()`.
+                -- animation = --<function: implements constant 20ms between steps>,
+                -- Symbol priority. Increase to display on top of more symbols.
+                priority = 2
+            },
 
-				-- Go to hunk range in corresponding direction
-				goto_first = "[H",
-				goto_prev = "[h",
-				goto_next = "]h",
-				goto_last = "]H",
-			},
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                -- Textobjects
+                object_scope = "ii",
+                object_scope_with_border = "ai",
 
-			-- Various options
-			options = {
-				-- Diff algorithm. See `:h vim.diff()`.
-				algorithm = "histogram",
+                -- Motions (jump to respective border line; if not present - body line)
+                goto_top = "[i",
+                goto_bottom = "]i"
+            },
 
-				-- Whether to use "indent heuristic". See `:h vim.diff()`.
-				indent_heuristic = true,
+            -- Options which control scope computation
+            options = {
+                -- Type of scope's border: which line(s) with smaller indent to
+                -- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+                border = "both",
 
-				-- The amount of second-stage diff to align lines (in Neovim>=0.9)
-				linematch = 60,
+                -- Whether to use cursor column when computing reference indent.
+                -- Useful to see incremental scopes with horizontal cursor movements.
+                indent_at_cursor = true,
 
-				-- Whether to wrap around edges during hunk navigation
-				wrap_goto = false,
-			},
-		})
-		-- Render Indent Guides
-		require("mini.indentscope").setup({
-			-- Draw options
-			draw = {
-				-- Delay (in ms) between event and start of drawing scope indicator
-				delay = 100,
+                -- Whether to first check input line to be a border of adjacent scope.
+                -- Use it if you want to place cursor on function header to get scope of
+                -- its body.
+                try_as_border = false
+            },
 
-				-- Animation rule for scope's first drawing. A function which, given
-				-- next and total step numbers, returns wait time (in ms). See
-				-- |MiniIndentscope.gen_animation| for builtin options. To disable
-				-- animation, use `require('mini.indentscope').gen_animation.none()`.
-				-- animation = --<function: implements constant 20ms between steps>,
-				-- Symbol priority. Increase to display on top of more symbols.
-				priority = 2,
-			},
+            -- Which character to use for drawing scope indicator
+            symbol = "╎"
+        })
+        -- Auto-complete paired characters
+        require("mini.pairs").setup({
+            -- In which modes mappings from this `config` should be created
+            modes = {
+                insert = true,
+                command = false,
+                terminal = false
+            },
 
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				-- Textobjects
-				object_scope = "ii",
-				object_scope_with_border = "ai",
+            -- Global mappings. Each right hand side should be a pair information, a
+            -- table with at least these fields (see more in |MiniPairs.map|):
+            -- - <action> - one of 'open', 'close', 'closeopen'.
+            -- - <pair> - two character string for pair to be used.
+            -- By default pair is not inserted after `\`, quotes are not recognized by
+            -- `<CR>`, `'` does not insert pair after a letter.
+            -- Only parts of tables can be tweaked (others will use these defaults).
+            mappings = {
+                ["("] = {
+                    action = "open",
+                    pair = "()",
+                    neigh_pattern = "[^\\]."
+                },
+                ["["] = {
+                    action = "open",
+                    pair = "[]",
+                    neigh_pattern = "[^\\]."
+                },
+                ["{"] = {
+                    action = "open",
+                    pair = "{}",
+                    neigh_pattern = "[^\\]."
+                },
 
-				-- Motions (jump to respective border line; if not present - body line)
-				goto_top = "[i",
-				goto_bottom = "]i",
-			},
+                [")"] = {
+                    action = "close",
+                    pair = "()",
+                    neigh_pattern = "[^\\]."
+                },
+                ["]"] = {
+                    action = "close",
+                    pair = "[]",
+                    neigh_pattern = "[^\\]."
+                },
+                ["}"] = {
+                    action = "close",
+                    pair = "{}",
+                    neigh_pattern = "[^\\]."
+                },
 
-			-- Options which control scope computation
-			options = {
-				-- Type of scope's border: which line(s) with smaller indent to
-				-- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
-				border = "both",
+                ['"'] = {
+                    action = "closeopen",
+                    pair = '""',
+                    neigh_pattern = "[^\\].",
+                    register = {
+                        cr = false
+                    }
+                },
+                ["'"] = {
+                    action = "closeopen",
+                    pair = "''",
+                    neigh_pattern = "[^%a\\].",
+                    register = {
+                        cr = false
+                    }
+                },
+                ["`"] = {
+                    action = "closeopen",
+                    pair = "``",
+                    neigh_pattern = "[^\\].",
+                    register = {
+                        cr = false
+                    }
+                }
+            }
+        })
 
-				-- Whether to use cursor column when computing reference indent.
-				-- Useful to see incremental scopes with horizontal cursor movements.
-				indent_at_cursor = true,
-
-				-- Whether to first check input line to be a border of adjacent scope.
-				-- Use it if you want to place cursor on function header to get scope of
-				-- its body.
-				try_as_border = false,
-			},
-
-			-- Which character to use for drawing scope indicator
-			symbol = "╎",
-		})
-		-- Auto-complete paired characters
-		require("mini.pairs").setup({
-			-- In which modes mappings from this `config` should be created
-			modes = { insert = true, command = false, terminal = false },
-
-			-- Global mappings. Each right hand side should be a pair information, a
-			-- table with at least these fields (see more in |MiniPairs.map|):
-			-- - <action> - one of 'open', 'close', 'closeopen'.
-			-- - <pair> - two character string for pair to be used.
-			-- By default pair is not inserted after `\`, quotes are not recognized by
-			-- `<CR>`, `'` does not insert pair after a letter.
-			-- Only parts of tables can be tweaked (others will use these defaults).
-			mappings = {
-				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-
-				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-
-				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
-			},
-		})
-
-		-- ... and there is more!
-		--  Check out: https://github.com/echasnovski/mini.nvim
-	end,
+        -- ... and there is more!
+        --  Check out: https://github.com/echasnovski/mini.nvim
+    end
 }
 
 return mini
