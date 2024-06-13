693539b2ce885c5b45ea4254c1dbd6447a4aaae9
DrInfinite <work.naparajith@gmail.com>
2024-06-13 11:48:18 +0530

    fix(refactor): mini.lua

--- lua/bootstrap/basic/mini.lua
+++ lua/bootstrap/basic/mini.lua
@@ -1,13 +1,7 @@
 local mini = { -- Collection of various small independent plugins/modules
-    "echasnovski/mini.nvim",
-    config = function()
-        require("lua.bootstrap.basic.mini.ai")
-        require("lua.bootstrap.basic.mini.diff")
-        require("lua.bootstrap.basic.mini.indentscope")
-        require("lua.bootstrap.basic.mini.pairs")
-        -- ... and there is more!
-        --  Check out: https://github.com/echasnovski/mini.nvim
-    end
+require("lua.bootstrap.basic.mini.ai"), require("lua.bootstrap.basic.mini.diff"),
+require("lua.bootstrap.basic.mini.indentscope"), require("lua.bootstrap.basic.mini.pairs") -- ... and there is more!
+--  Check out: https://github.com/echasnovski/mini.nvim
 }
 
 return mini
